80202330dcb4f0527d834aa66d32eeef46e48d69
Danilo Santos <116808289+DSantos29@users.noreply.github.com>
2023-04-20 20:30:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const port = 3000
 app.use(express.json());
 app.use(cors())
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "usuariodb.onrender.com/"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 
 UsauarioController.getUsauarioController(app)
 
